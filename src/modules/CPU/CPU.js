e7ddf960d0d6ade7d3c9067ac1a444604fccf1d0
Juan Navarro <jnavcamacho@gmail.com>
2021-04-15 14:05:49 -0600

    se elimina código en cPU que no se ocupa

--- src/modules/CPU/CPU.js
+++ src/modules/CPU/CPU.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Panel } from 'rsuite';
-import InstructorGenerator from '../Core/InstructionGenerator';
 import models from '../Core/models';
-import { fetch } from './CPUSlice';
-import { useDispatch } from 'react-redux';
 
 import './CPU.css';
 /**
  * Esta clase abstrae la simulación de 
  */
 export const CPU = (props) => {
-  //const dispatch = useDispatch();
   const dataInstruction = useSelector(state => state.CPUs.value[props.id]);
-  console.log(dataInstruction);
-  // const fetchInstructions = () => {
-
-  //   const generator = new InstructorGenerator();
-  //   setInterval(() => {
-  //     let instruction = generator.generateInstruction();
-  //     console.log('for CP1 ', props.id);
-  //     // dispatch(fetch({ id: props.id, ...instruction }));
-  //     // setInstruction({ instruction: generator.generateInstruction() });
-  //   }, 10000);
-
-  // }
-  // fetchInstructions();
+  //console.log(dataInstruction);
   const DisplayOperation = (dProps) => {
     const data = dProps.instruction;
     if (data) {
