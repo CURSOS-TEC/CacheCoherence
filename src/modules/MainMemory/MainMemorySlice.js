30769cf2f4a7aa7e6f00366fb4422b70db579420
Juan Navarro <jnavcamacho@gmail.com>
2021-04-15 22:58:59 -0600

    se limpia la memoria ram

--- src/modules/MainMemory/MainMemorySlice.js
+++ src/modules/MainMemory/MainMemorySlice.js
@@ -8,49 +8,49 @@ export const mainMemorySlice = createSlice({
                 block: '0',
                 state: 'N/A',
                 address: '0x1',
-                data: '0x9F'
+                data: '0x00'
             },
             {
                 block: '1',
                 state: 'DS',
-                address: '0x23',
-                data: '0x9F'
+                address: '0x2',
+                data: '0x00'
             },
             {
                 block: '2',
                 state: 'N/A',
-                address: '0x1',
-                data: '0x9F'
+                address: '0x3',
+                data: '0x00'
             },
             {
                 block: '3',
                 state: 'DI',
-                address: '0x23',
-                data: '0x9F'
+                address: '0x4',
+                data: '0x00'
             },
             {
                 block: '4',
                 state: 'N/A',
-                address: '0x1',
-                data: '0x9F'
+                address: '0x5',
+                data: '0x00'
             },
             {
                 block: '5',
                 state: 'DS',
-                address: '0x23',
-                data: '0x9F'
+                address: '0x6',
+                data: '0x00'
             },
             {
                 block: '6',
                 state: 'N/A',
-                address: '0x1',
-                data: '0x9F'
+                address: '0x7',
+                data: '0x00'
             },
             {
                 block: '7',
                 state: 'DI',
-                address: '0x23',
-                data: '0x9F'
+                address: '0x8',
+                data: '0x00'
             }
         ],
     },
